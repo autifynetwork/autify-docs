271468d28e694a95b6ccfb2d0a2e916297167a0a
Nabarun Chakraborty <nabarunchakraborty@Nabaruns-MacMini.local>
2025-07-22 19:11:48 +0530

    Serve docs at root URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,23 +1,25 @@
-
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const config = {
   title: 'Autify Network Docs',
   tagline: 'Documentation for Autify Network',
-  url: 'https://docs.autifynetwork.com',
-  baseUrl: '/',
+  url: 'https://docs.autifynetwork.com', // Your website URL
+  baseUrl: '/', // Serves docs at root
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'autifynetwork',
-  projectName: 'autify-docs',
+  favicon: 'img/favicon.ico', // Make sure this file exists under static/img/
+
+  organizationName: 'autifynetwork', // GitHub org/user name
+  projectName: 'autify-docs', // GitHub repo name
+
   presets: [
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', // Serve docs at root
           sidebarPath: require.resolve('./sidebars.js'),
         },
         theme: {
@@ -29,3 +31,4 @@ const config = {
 };
 
 module.exports = config;
+
